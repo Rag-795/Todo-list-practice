ce0dea889cbb0124c6f0605961b42a0d57af716c
RAGHAV N <161674308+Rag-795@users.noreply.github.com>
2024-04-23 13:12:48 +0530

    Add files via upload

---
+++ scripts/todo-2.js
@@ -0,0 +1,26 @@
+const todoList =[];
+
+function showTodo()
+{
+    let todoListHTML = '';
+    
+    for(let i=0;i<todoList.length;i++)
+    {
+        const todo = todoList[i];
+        const html = `<p>${todo}</p>`;
+        todoListHTML += html;
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+function addTodo()
+{
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+    
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value='';
+    showTodo();
+}
